fa310fd6029505ea66caa96e90ab3718e6f16e21
OgataiKhan <badellino.kevin@gmail.com>
2023-11-15 14:56:34 +0100

    feat: fill grid

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
-function myCreateElement(tag, className, content){
+function myCreateElement(tag, classNames, content){
     const element = document.createElement(tag);
-    element.classList.add(className);
+    element.classList.add(...classNames);
     element.append(content);
     return element;
 }
 
+const board = document.querySelector('.board');
+
+const classArray = ['cell', 'cell10'];
+
+for (let i = 1; i <= 100; i++) {
+    const myCell = myCreateElement('div', classArray, i);
+    board.append(myCell);
+}
