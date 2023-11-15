07e2cde63ebcf53860ae8c89850a88110f3de538
OgataiKhan <badellino.kevin@gmail.com>
2023-11-15 15:17:30 +0100

    feat: handle button interactivity

--- js/main.js
+++ js/main.js
@@ -8,16 +8,26 @@ function myCreateElement(tag, classNames, content){
     return element;
 }
 
+function handlePlayClick () {
+    // Clear existing grid
+    board.innerHTML = '';
+
+    // Grid creation
+    for (let i = 1; i <= 100; i++) {
+        const myCell = myCreateElement('div', classArray, i);
+        // Cell event listener
+        myCell.addEventListener('click', function() {
+        console.log(i);
+        this.classList.add('cell-clicked');
+        });
+        board.append(myCell);
+    }
+}
+
 const board = document.querySelector('.board');
 const classArray = ['cell', 'cell10'];
+const playButtons = document.querySelectorAll('.play-btn');
 
-// Grid creation
-for (let i = 1; i <= 100; i++) {
-    const myCell = myCreateElement('div', classArray, i);
-    // Event listener
-    myCell.addEventListener('click', function() {
-    console.log(i);
-    this.classList.add('cell-clicked');
-    });
-    board.append(myCell);
+for (let j = 0; j < playButtons.length; j++) {
+    playButtons[j].addEventListener('click', handlePlayClick);
 }
