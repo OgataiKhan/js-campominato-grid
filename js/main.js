c92f9b25c9cbebc0e0f7a5b50ed523d85049b8ef
OgataiKhan <badellino.kevin@gmail.com>
2023-11-15 14:56:52 +0100

    feat: add event listener

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Functions
 function myCreateElement(tag, classNames, content){
     const element = document.createElement(tag);
     element.classList.add(...classNames);
@@ -8,10 +9,15 @@ function myCreateElement(tag, classNames, content){
 }
 
 const board = document.querySelector('.board');
-
 const classArray = ['cell', 'cell10'];
 
+// Grid creation
 for (let i = 1; i <= 100; i++) {
     const myCell = myCreateElement('div', classArray, i);
+    // Event listener
+    myCell.addEventListener('click', function() {
+    console.log(i);
+    this.classList.add('cell-clicked');
+    });
     board.append(myCell);
 }
