d9f21528a8eae508969c484ed66236a30e3d5dad
OgataiKhan <badellino.kevin@gmail.com>
2023-11-15 14:38:26 +0100

    feat: my create element function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 'use strict';
 
+function myCreateElement(tag, className, content){
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.append(content);
+    return element;
+}
+
